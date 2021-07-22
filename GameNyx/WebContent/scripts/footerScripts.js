837b06b0897deff5750aecbd6a74c844939465b0
niky-hat <cappellonicola04@gmail.com>
2021-07-22 15:06:31 +0200

    Modificato footer;

---
+++ GameNyx/WebContent/scripts/footerScripts.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+ 
+ function showChiSiamo()
+ {
+ 	var chiSiamo=document.getElementById("chiSiamoDiv").style.display="block";
+ 	var Contattaci=document.getElementById("contattaciDiv").style.display="none";
+ }
+ 
+ function showContattaci()
+ {
+ 	var chiSiamo=document.getElementById("chiSiamoDiv").style.display="none";
+ 	var Contattaci=document.getElementById("contattaciDiv").style.display="block";
+ }
